70f994a086656869d3630798d5d7abed6add261e
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2018-04-09 17:32:02 -0700

    Add markdown heading anchors to docs website.

--- docs/js/main.js
+++ docs/js/main.js
@@ -29,6 +29,38 @@ $(function() {
     // Markdown plain out to bootstrap style
     $('#markdown-content-container table').addClass('table');
     $('#markdown-content-container img').addClass('img-responsive');
-
-
 });
+
+///
+// Add Markdown Heading Anchors
+// (source: http://blog.parkermoore.de/2014/08/01/header-anchor-links-in-vanilla-javascript-for-github-pages-and-jekyll/)
+
+var anchorForId = function (id) {
+  var anchor = document.createElement("a");
+  anchor.className = "header-link";
+  anchor.href      = "#" + id;
+  anchor.innerHTML = "<i class=\"fa fa-link\"></i>";
+  return anchor;
+};
+
+var linkifyAnchors = function (level, containingElement) {
+  var headers = containingElement.getElementsByTagName("h" + level);
+  for (var h = 0; h < headers.length; h++) {
+    var header = headers[h];
+
+    if (typeof header.id !== "undefined" && header.id !== "") {
+      header.appendChild(anchorForId(header.id));
+    }
+  }
+};
+
+document.onreadystatechange = function () {
+  if (this.readyState === "complete") {
+    var contentBlock = document.getElementById("markdown-content-container");
+    if (contentBlock) {
+      for (var level = 1; level <= 6; level++) {
+        linkifyAnchors(level, contentBlock);
+      }
+    }
+  }
+};
