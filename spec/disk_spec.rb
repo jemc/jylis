e49d1884db751ecdbb48960e8670632472a45f65
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2018-05-21 19:07:42 -0700

    Add spec for transferring a single disk persistence file.

--- spec/disk_spec.rb
+++ spec/disk_spec.rb
@@ -42,4 +42,28 @@ describe "Disk persistence" do
          *2 *3 :0 *0 *0 *2 $4 key2 *2 $3 bar :8
          *2 *3 :0 *0 *0 *2 $4 key3 *2 $3 baz :9].join("\r\n") + "\r\n"
   end
+  
+  it "allows an individual append-only file to be transferred/restored" do
+    # Create a source database and write some data to it.
+    source = Jylis.new(disk_dir: "#{disk_dir}-source")
+    source.run do
+      source.call(%w[TREG SET key1 foo 7]).should eq "OK"
+    end
+    
+    # Expect not to be able to read the data on the other database yet.
+    subject.run do
+      subject.call(%w[TREG GET key1]).should eq ["", 0]
+    end
+    
+    # Copy the persisted data from the source database to the other database.
+    FileUtils.copy(
+      File.join(source.opts[:disk_dir],  "append.TREG.jylis"),
+      File.join(subject.opts[:disk_dir], "append.TREG.jylis"),
+    )
+    
+    # Expect the data to now be readable from the other database.
+    subject.run do
+      subject.call(%w[TREG GET key1]).should eq ["foo", 7]
+    end
+  end
 end
